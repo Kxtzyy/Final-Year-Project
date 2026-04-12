ce149c6bf9cd873f6fcd933517e999d0dcbd78b7
Yash Krishnan <yash.krishnan123@gmail.com>
2026-04-12 15:11:14 +0100

    Added data manipulation functions for users Table

--- database/db.py
+++ database/db.py
@@ -21,3 +21,34 @@ async def init_db():
             content             TEXT        NOT NULL
             );
         """)
+        await db.commit()
+async def create_user(username: str, password: str) -> dict | None:
+    try:
+        async with aiosqlite.connect(DB_PATH) as db:
+            cursor = await db.execute(
+                "INSERT INTO users (username, password) VALUES (?, ?)",
+                (username, password)
+            )
+            await db.commit()
+            return {"id": cursor.lastrowid, "username": username}
+    except aiosqlite.IntegrityError:
+        return None
+
+async def get_user(username: str, password: str) -> dict | None:
+    async with aiosqlite.connect(DB_PATH) as db:
+        db.row_factory = aiosqlite.Row
+        cursor = await db.execute(
+            "SELECT * FROM users WHERE username = ? AND password = ?",
+            (username, password)
+        )
+        row = await cursor.fetchone()
+        return dict(row) if row else None
+
+async def del_user(username: str, password: str) -> bool:
+    async with aiosqlite.connect(DB_PATH) as db:
+        cursor = await db.execute(
+            "DELETE FROM users WHERE username = ? AND password = ?",
+            (username, password)
+        )
+        await db.commit()
+        return cursor.rowcount > 0
--- database/dbtest.py
+++ database/dbtest.py
@@ -1,9 +1,11 @@
 import asyncio
 
-from db import init_db
+import db
 
 async def test():
-    await init_db()
+    await db.init_db()
     print("Initialised")
-
+    #await db.create_user(username="Yash", password="123")
+    await db.del_user(username="Yash", password="123")
+    print (await db.get_user(username="Yash", password="123"))
 asyncio.run(test())
